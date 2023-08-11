d5d1f01c5877b402dacb24f181331092289a9148
EthanOk <sdzdj01@163.com>
2023-08-11 18:02:36 +0800

    modified:   contracts/YunGouActivity/LuckyBaby.sol

--- contracts/YunGouActivity/LuckyBaby.sol
+++ contracts/YunGouActivity/LuckyBaby.sol
@@ -3,11 +3,12 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 
-contract LuckyBaby is AccessControl, Pausable, ReentrancyGuard {
+contract LuckyBaby is AccessControl, Pausable, ReentrancyGuard, ERC721Holder {
     bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
     bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
 
@@ -19,10 +20,12 @@ contract LuckyBaby is AccessControl, Pausable, ReentrancyGuard {
 
     using Counters for Counters.Counter;
     Counters.Counter public currentIssueId;
+
     enum PayType {
         NATIVE,
         ERC20
     }
+
     enum PrizeType {
         NATIVE,
         ERC20,
@@ -37,12 +40,13 @@ contract LuckyBaby is AccessControl, Pausable, ReentrancyGuard {
 
     struct Prize {
         PrizeType prizeType;
+        uint64 numberWinner;
         address token;
-        uint256 amount;
+        uint256[] amounts;
         uint256[] tokenIds;
     }
 
-    struct issueData {
+    struct IssueData {
         uint64 numberCurrent;
         uint64 numberMax;
         uint64 countMaxPer;
@@ -53,26 +57,45 @@ contract LuckyBaby is AccessControl, Pausable, ReentrancyGuard {
         Prize prize;
     }
 
-    struct issueAccount {
+    struct IssueAccount {
         address[] participants;
         address[] winners;
+    }
+
+    struct AccountState {
+        uint64 countPart;
+        uint64 index;
+        bool stateWinner;
+        bool stateRedeem;
     }
 
     event Participate(
         address indexed account,
         uint256 indexed issueId,
         uint256 count,
-        uint256 time
+        uint256 timeParticipate
+    );
+
+    event RedeemPrize(
+        address indexed account,
+        uint256 indexed issueId,
+        PrizeType prizeType,
+        address token,
+        uint256 amount,
+        uint256[] tokenIds,
+        uint256 timeRedeem
     );
 
     // issueId => issueData
-    mapping(uint256 => issueData) public issueDatas;
+    mapping(uint256 => IssueData) public issueDatas;
 
     // issueId => issueAccount
-    mapping(uint256 => issueAccount) private issueAccounts;
+    mapping(uint256 => IssueAccount) private issueAccounts;
 
-    // account => issueId => count
-    mapping(address => mapping(uint256 => uint256)) public participationCount;
+    // account => issueId => AccountState
+    mapping(address => mapping(uint256 => AccountState)) public accountStates;
+
+    // account => issueId => winnerState
 
     constructor(address owner, address operator) {
         _setupRole(DEFAULT_ADMIN_ROLE, tx.origin);
@@ -97,7 +120,15 @@ contract LuckyBaby is AccessControl, Pausable, ReentrancyGuard {
         );
         require(startTime < endTime, "Invalid Time");
 
-        issueData storage _issueData = issueDatas[issueId];
+        require(prize.numberWinner > 0, "Invalid Number Winner");
+
+        if (prize.prizeType == PrizeType.ERC721) {
+            require(prize.tokenIds.length > 0, "Invalid Prize Data");
+        } else {
+            require(prize.amounts.length > 0, "Invalid Prize amounts");
+        }
+
+        IssueData storage _issueData = issueDatas[issueId];
         _issueData.numberMax = numberMax;
         _issueData.countMaxPer = countMaxPer;
         _issueData.startTime = startTime;
@@ -119,13 +150,15 @@ contract LuckyBaby is AccessControl, Pausable, ReentrancyGuard {
 
         require(startTime < endTime, "Invalid Time");
 
+        require(prize.numberWinner > 0, "Invalid Number Winner");
+
         if (prize.prizeType == PrizeType.ERC721) {
             require(prize.tokenIds.length > 0, "Invalid Prize Data");
         } else {
-            require(prize.amount > 0, "Invalid Prize Amount");
+            require(prize.amounts.length > 0, "Invalid Prize amounts");
         }
 
-        issueData storage _issueData = issueDatas[_issueId];
+        IssueData storage _issueData = issueDatas[_issueId];
         _issueData.numberMax = numberMax;
         _issueData.countMaxPer = countMaxPer;
         _issueData.startTime = startTime;
@@ -170,7 +203,7 @@ contract LuckyBaby is AccessControl, Pausable, ReentrancyGuard {
             "Invalid  IssueId"
         );
         uint256 _count = issueDatas[_issueId].countMaxPer -
-            participationCount[_account][_issueId];
+            accountStates[_account][_issueId].countPart;
 
         return _count;
     }
@@ -200,6 +233,7 @@ contract LuckyBaby is AccessControl, Pausable, ReentrancyGuard {
         uint256 count
     ) external payable whenNotPaused nonReentrant returns (bool) {
         address account = _msgSender();
+
         uint256 ethAmount = msg.value;
 
         require(
@@ -207,7 +241,7 @@ contract LuckyBaby is AccessControl, Pausable, ReentrancyGuard {
             "Invalid  IssueId"
         );
 
-        issueData storage _issueData = issueDatas[_issueId];
+        IssueData storage _issueData = issueDatas[_issueId];
 
         require(!_issueData.openState, "The Issue Already Awarded");
 
@@ -218,7 +252,7 @@ contract LuckyBaby is AccessControl, Pausable, ReentrancyGuard {
         );
 
         unchecked {
-            uint256 count_current = participationCount[account][_issueId] +
+            uint256 count_current = accountStates[account][_issueId].countPart +
                 count;
 
             require(
@@ -226,7 +260,7 @@ contract LuckyBaby is AccessControl, Pausable, ReentrancyGuard {
                 "Exceeding the Maximum Count Participation Per"
             );
 
-            participationCount[account][_issueId] = count_current;
+            accountStates[account][_issueId].countPart = uint64(count_current);
 
             uint256 _numberCurrent = _issueData.numberCurrent + count;
 
@@ -249,9 +283,183 @@ contract LuckyBaby is AccessControl, Pausable, ReentrancyGuard {
         return true;
     }
 
-    function _getRadom(uint256 salt) private view returns (uint256 _random) {
+    function redeemPrize(
+        uint256 _issueId
+    ) external whenNotPaused nonReentrant returns (bool) {
+        address account = _msgSender();
+
+        require(accountStates[account][_issueId].stateWinner, "Not A Winner");
+
+        require(
+            !accountStates[account][_issueId].stateRedeem,
+            "Already Redeem"
+        );
+
+        accountStates[account][_issueId].stateRedeem = true;
+
+        _distribute(account, _issueId);
+
+        return true;
+    }
+
+    function _distribute(address account, uint256 _issueId) private {
+        uint256 index = accountStates[account][_issueId].index;
+
+        Prize memory prize = issueDatas[_issueId].prize;
+
+        require(index < prize.amounts.length, "Invalid Index");
+
+        uint256 amount = prize.amounts[index];
+
+        if (prize.prizeType == PrizeType.NATIVE) {
+            uint256[] memory _tokenIds;
+            payable(account).transfer(amount);
+
+            emit RedeemPrize(
+                account,
+                _issueId,
+                prize.prizeType,
+                prize.token,
+                amount,
+                _tokenIds,
+                block.timestamp
+            );
+        } else if (prize.prizeType == PrizeType.ERC20) {
+            uint256[] memory _tokenIds;
+            _transferLowCall(prize.token, account, amount);
+
+            emit RedeemPrize(
+                account,
+                _issueId,
+                prize.prizeType,
+                prize.token,
+                amount,
+                _tokenIds,
+                block.timestamp
+            );
+        } else if (prize.prizeType == PrizeType.ERC721) {
+            uint256[] memory _tokenIds = _getTokenIds(
+                index,
+                prize.amounts,
+                prize.tokenIds
+            );
+
+            _batchTransferFrom(prize.token, account, _tokenIds);
+
+            emit RedeemPrize(
+                account,
+                _issueId,
+                prize.prizeType,
+                prize.token,
+                amount,
+                _tokenIds,
+                block.timestamp
+            );
+        }
+    }
+
+    function _getTokenIds(
+        uint256 index,
+        uint256[] memory amounts,
+        uint256[] memory tokenIds
+    ) private pure returns (uint256[] memory) {
+        uint256 length = amounts[index];
+        uint256[] memory _tokenIds = new uint256[](length);
+
+        uint256 start = 0;
+
+        if (index > 0) {
+            for (uint256 j = 0; j < index; ++j) {
+                start += amounts[j];
+            }
+        }
+
+        for (uint256 j = 0; j < amounts[index]; ++j) {
+            _tokenIds[j] = tokenIds[j + start];
+        }
+
+        return _tokenIds;
+    }
+
+    function _batchTransferFrom(
+        address token,
+        address account,
+        uint256[] memory _tokenIds
+    ) private {
+        for (uint256 i = 0; i < _tokenIds.length; ++i) {
+            IERC721(token).transferFrom(address(this), account, _tokenIds[i]);
+        }
+    }
+
+    function openPrizePool(
+        uint256 _issueId
+    ) external onlyRole(OPERATOR_ROLE) returns (bool) {
+        IssueData storage _issueData = issueDatas[_issueId];
+
+        require(
+            block.timestamp > _issueData.endTime,
+            "Not Yet Time for Opening"
+        );
+        require(!_issueData.openState, "The Issue Already Opened");
+
+        _issueData.openState = true;
+
+        // Distribute Prizes
+        uint256 numberWinner = _issueData.prize.numberWinner;
+        address[] memory _participants = issueAccounts[_issueId].participants;
+        uint256 numberParticipant = _participants.length;
+        uint256 _number;
+        uint256 i;
+        unchecked {
+            while (_number < numberWinner) {
+                uint256 _random = _getRadom(
+                    i,
+                    _seed(_issueId),
+                    numberParticipant
+                );
+                address accountRadom = _participants[_random];
+
+                if (!accountStates[accountRadom][_issueId].stateWinner) {
+                    accountStates[accountRadom][_issueId].stateWinner = true;
+
+                    issueAccounts[_issueId].winners.push(accountRadom);
+
+                    accountStates[accountRadom][_issueId].index = uint64(
+                        _number
+                    );
+
+                    ++_number;
+                }
+                ++i;
+            }
+        }
+
+        return true;
+    }
+
+    function _getRadom(
+        uint256 index,
+        bytes32 seed_,
+        uint256 _modulus
+    ) private view returns (uint256) {
         // TODO:random
-        return (block.timestamp % 50) + (salt % 50);
+        uint256 rand = uint256(
+            keccak256(
+                abi.encodePacked(
+                    index,
+                    seed_,
+                    block.timestamp,
+                    block.prevrandao,
+                    block.coinbase
+                )
+            )
+        );
+        return rand % _modulus;
+    }
+
+    function _seed(uint256 _issueId) private view returns (bytes32) {
+        return
+            keccak256(abi.encodePacked(issueAccounts[_issueId].participants));
     }
 
     function _userPayToken(
@@ -289,6 +497,20 @@ contract LuckyBaby is AccessControl, Pausable, ReentrancyGuard {
     ) private {
         (bool success, bytes memory data) = target.call(
             abi.encodeWithSelector(ERC20_TRANSFERFROM_SELECTOR, from, to, value)
+        );
+        require(
+            success && (data.length == 0 || abi.decode(data, (bool))),
+            "Low-level call failed"
+        );
+    }
+
+    function _transferLowCall(
+        address target,
+        address to,
+        uint256 value
+    ) internal {
+        (bool success, bytes memory data) = target.call(
+            abi.encodeWithSelector(ERC20_TRANSFER_SELECTOR, to, value)
         );
         require(
             success && (data.length == 0 || abi.decode(data, (bool))),
