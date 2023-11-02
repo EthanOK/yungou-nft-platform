f417a05deb9af807f4ebefe3f0bcbfb2f057dfd4
EthanOk <sdzdj01@163.com>
2023-11-02 11:38:56 +0800

    modified:   scripts/deploy_CrossChainYGInBSC.js

--- scripts/deploy_CrossChainYGInBSC.js
+++ scripts/deploy_CrossChainYGInBSC.js
@@ -24,4 +24,7 @@ main().catch((error) => {
 
 // npx hardhat run scripts/deploy_CrossChainYGInBSC.js --network tbsc
 
-// npx hardhat verify --network tbsc 0x2fb2738Fa40B993166B40EbeED06B6d2a575cB45 0x0Fa4640F99f876D78Fc964AFE0DD6649e7C23c4f 0xDb6c494BE6Aae80cc042f9CDA24Ce573aD163A46 0x5ab85B15e0ED0009A8AA606cb07809230fC16eaA
+// npx hardhat verify --network tbsc 0xa47738eaa7B1daf9Bd1f438afc8CCC4661e69a3C 0x0Fa4640F99f876D78Fc964AFE0DD6649e7C23c4f 0xDb6c494BE6Aae80cc042f9CDA24Ce573aD163A46 0x5ab85B15e0ED0009A8AA606cb07809230fC16eaA
+
+// TODO:
+// 部署成功后，将Cross合约地址 设置 为ygio和ygme 白名单
