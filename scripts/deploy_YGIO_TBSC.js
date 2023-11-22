80ec295836190f7d802931b48701e3b3ee2f53b0
EthanOK <sdzdj01@163.com>
2023-11-22 20:33:41 +0800

    modified:   scripts/deploy_YGIO_TBSC.js

--- scripts/deploy_YGIO_TBSC.js
+++ scripts/deploy_YGIO_TBSC.js
@@ -1,8 +1,8 @@
 const hre = require("hardhat");
 
 async function main() {
-  await ygio_tbsc();
-  //  await ygio_bsc();
+  // await ygio_tbsc();
+  await ygio_bsc();
 }
 
 async function ygio_tbsc() {
@@ -40,7 +40,7 @@ main().catch((error) => {
 // Cross Chain: 0xaB4803501d26364150a4d3Cd029b8354F6dc9f3D
 
 // npx hardhat run scripts/deploy_YGIO_TBSC.js --network tbsc
-// npx hardhat verify --network tbsc 0x071589C3d21CB744321EF98c55322E3b85F11c73 0xC675897Bb91797EaeA7584F025A5533DBB13A000 0xaB4803501d26364150a4d3Cd029b8354F6dc9f3D
+// npx hardhat verify --network tbsc 0x071589C3d21CB744321EF98c55322E3b85F11c73 0x6278A1E803A76796a3A1f7F6344fE874ebfe94B2 0xaB4803501d26364150a4d3Cd029b8354F6dc9f3D
 
 // npx hardhat run scripts/deploy_YGIO_TBSC.js --network bsc
 // npx hardhat verify --network bsc 0xa2FCACCDCf80Ab826e3Da6831dA711E7c85C6F67 0xC675897Bb91797EaeA7584F025A5533DBB13A000 0xaB4803501d26364150a4d3Cd029b8354F6dc9f3D
